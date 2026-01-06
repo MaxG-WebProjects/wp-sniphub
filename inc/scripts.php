504598666a56b56a37a3a5f9ff8d5b8f95d2d0c3
MaxG-WebProjects <228169503+MaxG-WebProjects@users.noreply.github.com>
2026-01-06 10:20:23 +0100

    Update scripts.php

--- inc/scripts.php
+++ inc/scripts.php
@@ -3,12 +3,15 @@
  * Chargement des scripts et styles
  */
 
-if ( ! defined( 'ABSPATH' ) ) {
-	 exit;
+/*if ( ! defined( 'ABSPATH' ) ) {
+     exit;
  }
  
  function theme_enqueue_assets() {
-	 wp_enqueue_style( 'theme-style', get_stylesheet_uri(), [], wp_get_theme()->get( 'Version' ) );
-	 wp_enqueue_script( 'theme-script', get_template_directory_uri() . '/js/main.js', ['jquery'], wp_get_theme()->get( 'Version' ), true );
+     wp_enqueue_style( 'theme-style', get_stylesheet_uri(), [], wp_get_theme()->get( 'Version' ) );
+     wp_enqueue_script( 'theme-script', get_template_directory_uri() . '/js/main.js', ['jquery'], wp_get_theme()->get( 'Version' ), true );
  }
  add_action( 'wp_enqueue_scripts', 'theme_enqueue_assets', 10 );
+ */
+ 
+ ////////////////////////////////////////////////////
